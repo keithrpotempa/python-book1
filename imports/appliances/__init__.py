9ce45fd30a03f28703f7d72a7b35bce00c5371e3
keithrpotempa <keithrpotempa@gmail.com>
2020-04-15 15:13:21 -0500

    completed exercises for ch13

--- imports/appliances/__init__.py
+++ imports/appliances/__init__.py
@@ -1,3 +1,9 @@
-from .appliance import Appliance
+from appliances.appliance import Appliance
+
 from appliances.kitchen.utility.dishwasher import DishWasher
-#from kitchen.can_opener import CanOpener
+from appliances.kitchen.utility.refrigerator import Refrigerator
+from appliances.kitchen.coffeemaker import CoffeeMaker
+from appliances.kitchen.can_opener import CanOpener
+
+from appliances.laundry.washer import Washer
+from appliances.laundry.dryer import Dryer
--- imports/index.py
+++ imports/index.py
@@ -1,13 +1,19 @@
-from appliances import Dishwasher #Dryer, Washer, Refrigerator
+from appliances import Appliance, DishWasher, Washer, Dryer, Refrigerator, CoffeeMaker, CanOpener
 
-whirlpool_dishwasher = Dishwasher("black")
+whirlpool_dishwasher = DishWasher("black")
 whirlpool_dishwasher.wash_dishes()
 
 samsung_washer = Washer("red")
+samsung_washer.wash_clothes()
+
 samsung_dryer = Dryer("red", "gas")
+samsung_dryer.dry_clothes("high")
 
 lg_fridge = Refrigerator("stainless")
 lg_fridge.make_ice()
 
 mr_coffee = CoffeeMaker("white")
 mr_coffee.make_coffee()
+
+brand_can_opener = CanOpener("black")
+brand_can_opener.open_can()
